6c2bbb85f20248eafe15270fa32a9748b5a5ab00
Edgar Bezhanyan <edgar.bezhanyan@gmail.com>
2023-08-07 20:16:46 +0400

    change .gitignore

---
+++ Plugins/Rocket/Source/Rocket/Private/RocketSpawnSocket.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RocketSpawnSocket.h"
+
+// Sets default values
+ARocketSpawnSocket::ARocketSpawnSocket()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Init();
+}
+
+// Called when the game starts or when spawned
+void ARocketSpawnSocket::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void ARocketSpawnSocket::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ARocketSpawnSocket::Init()
+{
+	m_mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	m_mesh->SetupAttachment(GetRootComponent());
+	Arrow = CreateDefaultSubobject<UArrowComponent>("Arrow");
+	Arrow->SetupAttachment(GetRootComponent());
+
+}
+
+void ARocketSpawnSocket::Fire(FVector start, FRotator rot)
+{
+	FHitResult OutHit = LineTrace(start, UKismetMathLibrary::GetForwardVector(rot));
+	UPrimitiveComponent* HitComponent = OutHit.GetComponent();
+	SpawnARocketProjectile(HitComponent);
+}
+
+
+
+FHitResult ARocketSpawnSocket::LineTrace(FVector start, FVector forwardVector)
+{
+	FHitResult OutHit;
+	FVector End = ((forwardVector * 100000.f) + start);
+	FCollisionQueryParams CollisionParams;
+
+	DrawDebugLine(GetWorld(), start, End, FColor::Green, false, 1, 0, 1);
+
+	if (GetWorld()->LineTraceSingleByChannel(OutHit, start, End, ECC_Visibility, CollisionParams))
+	{
+		if (OutHit.bBlockingHit)
+		{
+			if (GEngine) {
+
+				GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("You are hitting: %s"), *OutHit.GetActor()->GetName()));
+				GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("Impact Point: %s"), *OutHit.ImpactPoint.ToString()));
+				GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("Normal Point: %s"), *OutHit.ImpactNormal.ToString()));
+
+			}
+		}
+	}
+	return OutHit;
+}
+
+void ARocketSpawnSocket::SpawnARocketProjectile(UPrimitiveComponent* HitComponent)
+{
+	FTransform SpawnLocAndRotation = GetActorTransform();
+	SpawnLocAndRotation.SetLocation(GetActorLocation() + (UKismetMathLibrary::GetForwardVector(GetActorRotation()) * 200));
+	ARocketProjectile* SpawnedActorRef = GetWorld()->SpawnActorDeferred<ARocketProjectile>(ActorToSpawn, SpawnLocAndRotation);
+	SpawnedActorRef->ExposeOnSpawnHitComponent(HitComponent,Arrow);
+	SpawnedActorRef->FinishSpawning(SpawnLocAndRotation);
+
+}
+
---
+++ Plugins/Rocket/Source/Rocket/Public/RocketSpawnSocket.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "DrawDebugHelpers.h"
+#include "RocketProjectile.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Components/ArrowComponent.h"
+
+
+#include "RocketSpawnSocket.generated.h"
+
+
+
+UCLASS()
+class ROCKET_API ARocketSpawnSocket : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ARocketSpawnSocket();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	// functions
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable)
+	void Fire(FVector start, FRotator rot);
+private: // functions
+	UFUNCTION()
+		void Init();
+	UFUNCTION()
+		FHitResult LineTrace(FVector start, FVector forwardVector);
+	UFUNCTION()
+		void SpawnARocketProjectile(UPrimitiveComponent* HitComponent);
+		
+public: // members
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* m_mesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+		TSubclassOf<ARocketProjectile> ActorToSpawn;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+		UArrowComponent* Arrow;
+};
